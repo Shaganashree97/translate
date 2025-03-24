f1b7798c690877fb600043d6b218971865de5840
Shaganashree P L <shaganashree.pl@gmail.com>
2025-03-24 20:29:31 +0530

    languages in translation

---
+++ language.py
@@ -0,0 +1,10 @@
+# To Print all the languages that google
+# translator supports
+import googletrans
+
+
+print(googletrans.LANGUAGES)
+print(len(googletrans.LANGUAGES))
+print(type(googletrans.LANGUAGES))
+print(googletrans.LANGUAGES['en'])
+print(googletrans.LANGUAGES['ta'])
